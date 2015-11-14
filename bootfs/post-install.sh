6d8bab002328181a1901238cf7cca9be21862c99
Dayton <daytonpid@gmail.com>
2015-11-14 00:32:07 -0700

    Added licence and prepped
    Former-commit-id: cdf1f4b0d5bee7d54aa68669cb6119cfbf2cf6ce [formerly 5cca53a8c06b66156b88e10c2b6ce99532fb765e]
    Former-commit-id: 8ebf12a85f8c9f839eeae10c7cdd640cb48af55d

--- bootfs/post-install.sh
+++ bootfs/post-install.sh
@@ -1,45 +1,58 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2015 daytonpid@gmail.com
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # post-install.txt
 
-if [ "$1" == "install" ]
-then
+set -e
 
-	set -e
+echo "post-install - STAGE 0"
+	apt-get -y install raspi-copies-and-fills raspi-config libraspberrypi-bin btrfs-tools apt-utils rpi-update git unzip
+	
 
-	echo "post-install - STAGE 0"
-		dpkg-reconfigure locales
-		dpkg-reconfigure tzdata
+echo "post-install - STAGE 1"
+	btrfs subvolume create /.snapshot
+	btrfs subvolume snapshot -r / /.snapshot/base-system/
 
-	echo "post-install - STAGE 1"
-		apt-get -y install raspi-copies-and-fills raspi-config libraspberrypi-bin btrfs-tools apt-utils rpi-update
-		btrfs subvolume create /.snapshot
-		btrfs subvolume snapshot -r / /.snapshot/base-system/
+echo "post-install - STAGE 2"
+	rpi-update
+	cp -v /boot/vmlinuz-* /boot/kernel.img
 
-	echo "post-install - STAGE 2"
-		rpi-update
-		cp -v /boot/vmlinuz-* /boot/kernel.img
+echo "post-install - STAGE 3" ## create a btrfs volume on a usb stick and add it to the fstab
+	mkfs.btrfs /dev/sda
+	(
+		echo
+		echo '/dev/sda        /data/local     btrfs   defaults 0 0'
+		echo
+	) >> /etc/fstab
+	mkdir -pv /data/local
+	mount -v  /data/local
+	btrfs subvolume create /data/local/.snapshot
 
-	echo "post-install - STAGE 4"
-		apt-get -y install git unzip
+echo "post-install - STAGE 4" ## **FINAL STEP FOR BETA TESTING**
+	btrfs subvolume snapshot -r / /.snapshot/configured-system
 
-	echo "post-install - STAGE 6"
-		mkfs.btrfs /dev/sda
-		(
-			echo
-			echo '/dev/sda        /data/local     btrfs   defaults 0 0'
-			echo
-		) >> /etc/fstab
-		mkdir -pv /data/local
-		mount -v  /data/local
-		btrfs subvolume create /data/local/.snapshot
-
-	echo "post-install - STAGE 7"
-		btrfs subvolume snapshot -r / /.snapshot/configured-system
-
-	echo "post-install - DONE"
-	echo "	sleeping 30 seconds ..."
-		sleep 30
-		shutdown -r now
-
-fi
+echo "post-install - DONE"
+echo "	sleeping 15 seconds then rebooting ..."
+	sleep 15
+	shutdown -r now
 
 # EOF
