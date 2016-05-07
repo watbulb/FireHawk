16efbf3bb15b22ae5c762b3a3cdedec51409c9ef
Dayton Pidhirney <daytonpid@gmail.com>
2016-05-07 17:09:35 -0600

    Cleaned up FireHawk welcome

--- recipes-misc/firehawk-welcome/files/firehawk-welcome.sh
+++ recipes-misc/firehawk-welcome/files/firehawk-welcome.sh
@@ -16,17 +16,10 @@ date=$( date "+%a %d %b" )
 
 # OS
 OS=$(uname -r)
-dist=$(cat /etc/*-release | grep -i name | cut -d'=' -d'"' -f2 | sed -n 1p)
 bit=$(uname -m)
 
 # Other
-UPT=`uptime | awk -F'( |,)' '{print $2}' | awk -F ":" '{print $1}'`
 uptime=$(uptime | sed 's/.*up \([^,]*\), .*/\1/')
-uptime2=$(uptime | sed -nr 's/.*\s+([[:digit:]]{1,2}):[[:digit:]]{2},.*/\1/p')
-size=$(df | grep '^/dev/[hs]d' | awk '{s+=$2} END {printf("%.0f\n", s/1048576)}')
-use=$(df | grep '^/dev/[hs]d' | awk -M '{s+=$3} END {printf("%.0f\n", s/1048576)}')
-gb=$(echo "G")
-memkb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 ram=$(expr $memkb / 1024)
 
 cat << EOF
@@ -39,5 +32,6 @@ $f3 |_|    |_||_|   \___||_| |_| \__,_|  \_/\_/  |_|\_\
 $f3
 $f6             $USER $f7@ $f1$HOSTNAME
 $f6            $H $f4$time$NC - $f7$date
-$f6         $dist with $f2$ram$f7 MB Memory
+$f6             $f2$ram$f7 MB Memory
+$f6              uptime: $f2$uptime
 EOF
